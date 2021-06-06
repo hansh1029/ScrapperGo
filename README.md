b4678878113aa6559b15b33c857e3baa22f50710
hansh1029 <hansh1029@gmail.com>
2021-06-06 15:17:14 +0900

    about golang

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ScrapperGo
+
+## Go
+- package, import, export(with using upper case)
+- type
+- naked return
+- defer
+- for(loop), range, args
+- pointers(& is address, * is value)
+- arrays and slices(arrays without length)
+- map and structs
--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
+type person struct {
+	name    string
+	age     int
+	favFood []string
+}
+
 func main() {
-	println("한글도 잘 나오나?")
+	favFood := []string{"kimchi", "ramen"}
+	nico := person{name: "nico", age: 18, favFood: favFood}
+	fmt.Println(nico.name)
 }
